dfbdd1b78d5e3645bf70f830f050f4c9c2060419
ddderi <maxime830@live.fr>
2022-03-11 00:28:44 +0100

    Done.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import TwitterMessage from "./components/TwitterMessage";
 
 class App extends Component {
 
-  login = ({ username, password }) => {
-    console.log(`Logging in ${username} with password ${password}`);
-  };
+  login = ({username, password}) => {
+    
+    console.log(`Logging in ${username} with password ${password}`);}
+  
+
+
+  
 
   render() {
     return (
