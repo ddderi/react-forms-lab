23da3bd9a933cf60f57ddc23cf3bb1530d0e3318
ddderi <maxime830@live.fr>
2022-03-11 00:28:39 +0100

    commit tree 4cdc19796167157bf8d2adcf7a1667f7e00b6255

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,34 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+         maxChars: 280,
+         
+    };
+  }
+
+  handleCharleft = (event) =>
+  {
+    
+    this.setState({
+      maxChars: this.state.maxChars - 1,
+      messageT:  event.target.value
+     
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.messageT} onChange={event => this.handleCharleft(event)} />
+        <p>{this.state.maxChars}</p>
+        <p>{this.state.messageT}</p>
       </div>
     );
   }
 }
+
+
 
 export default TwitterMessage;
